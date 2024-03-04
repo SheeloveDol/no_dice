81bf6c7a9f3b010776bc79b5eb1636b86c573574
Sheelove Dol <siesel26@gmail.com>
2024-03-04 10:28:14 -0800

    Refactored DicePageState to be more dynamic

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -18,8 +20,23 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
   const DicePage({super.key});
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  // Here we define the initial state of the dice. On the first run, the left dice will be 3.
+  int leftDiceNumber = 3;
+  int rightDiceNumber = 6;
+
+  // This method returns a random number between 1 and 6. We use it to change the state of the left dice.
+  int getRandomNumberForDice() {
+    final random = Random();
+    return random.nextInt(6) + 1;
+  }
 
   @override
   // The build method is called every time the state of our app changes. This allows us to change the UI of our app dynamically, and preview the changes in real time.
@@ -30,17 +47,27 @@ class DicePage extends StatelessWidget {
         Expanded(
           // Image.asset is the shorthand for Image(image: AssetImage('images/dice1.png'))
           child: TextButton(
-            child: Image.asset('images/dice1.png'),
+            child: Image.asset('images/dice$leftDiceNumber.png'),
             onPressed: () {
-              print('Left button got pressed.');
+              // Here we change the state of the left dice to a random number between 1 and 6.
+              // setState is a method that tells Flutter to rerun the build method with the updated state.
+              setState(() {
+                leftDiceNumber = getRandomNumberForDice();
+                rightDiceNumber = getRandomNumberForDice();
+              });
             },
           ),
         ),
         Expanded(
           child: TextButton(
-            child: Image.asset('images/dice2.png'),
+            child: Image.asset('images/dice$rightDiceNumber.png'),
             onPressed: () {
-              print('Right button got pressed.');
+              setState(
+                () {
+                  rightDiceNumber = getRandomNumberForDice();
+                  leftDiceNumber = getRandomNumberForDice();
+                },
+              );
             },
           ),
         ),
